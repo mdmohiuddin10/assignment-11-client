e7a8ee16d0927c5ff96338827c847b63f547e2cb
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2023-11-07 09:20:55 +0600

    added google fonts and created home route

--- src/Pages/Home/FeaturedFoods/FeaturedFoods.jsx
+++ src/Pages/Home/FeaturedFoods/FeaturedFoods.jsx
@@ -1,20 +1,35 @@
 import { useEffect, useState } from "react";
 
 
-const FeaturedFoods = () => {
-    const [food, setFood] = useState([])
 
-    useEffect(()=>{
+const FeaturedFoods = () => {
+
+    const [foods, setFoods] = useState([])
+    console.log(foods);
+
+    useEffect(() => {
         fetch('http://localhost:5000/allfoods')
-        .then(res => res.json())
-        .then(data => setFood(data))
-    },[])
-    
-    console.log(food);
+            .then(res => res.json())
+            .then(data => setFoods(data))
+
+    }, [])
+
 
     return (
-        <div>
-            <h2>food:{food.length} </h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-5 mt-10">
+          {
+            foods.map(food=>  <div key={food._id} className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={food.photo} alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>)
+          }
+
         </div>
     );
 };
--- src/Routs/Roots.jsx
+++ src/Routs/Roots.jsx
@@ -1,13 +1,17 @@
 
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/SharedRouts/Navbar/Navbar";
+import Footer from "../Pages/SharedRouts/Footer/Footer";
+
 
 
 const Roots = () => {
     return (
-        <div>
-         <Navbar></Navbar>
+        <div className="max-w-screen-xl mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
+        
         
         </div>
     );
