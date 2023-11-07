f37a8de866916c3fd14a697c1c8c1ac2f58bf293
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2023-11-07 11:07:01 +0600

    completed featute section and added add food input section

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,12 +1,99 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContex } from "../../firebase/AuthProvider";
 
 
 const Details = () => {
     const details = useLoaderData()
+    const {user} = useContext(AuthContex)
     console.log(details);
+
+    const getCurrentDate =()=>{
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+      }
+
+
+
     return (
-        <div>
-            <h2>hello</h2>
+        <div className="w-3/4 mx-auto">
+            <div className="card card-side bg-base-100 shadow-xl">
+                <figure><img className="" src={details.photo} alt="Movie" /></figure>
+                <div className="card-body">
+                    <p>pickup location:{details.pickupLocation}</p>
+                    <p>Donar Name:{details.name}</p>
+                    <p>Food Name: {details.foodName}</p>
+                    <p>Number Of server: {details.foodQuantity}</p>
+                    <p>Expire date: {details.date}</p>
+                    <div className="card-actions justify-end">
+                        {/* The button to open modal */}
+                        <a href="#my_modal_8" className="btn">request</a>
+                        {/* Put this part before </body> tag */}
+                        <div className="modal" id="my_modal_8">
+                            <div className="modal-box">
+                                <div className="form-control">
+                                    <label className="input-group">
+                                        <span>Food Name</span>
+                                        <input type="text" value={details.foodName} readOnly placeholder="info@site.com" className="input input-bordered" />
+                                    </label>
+                                </div><div className="form-control">
+                                    <label className="input-group">
+                                        <span>Food Image</span>
+                                        <input type="text" readOnly value={details.photo} placeholder="info@site.com" className="input input-bordered" />
+                                    </label>
+                                </div><div className="form-control">
+                                    <label className="input-group">
+                                        <span>Food Id</span>
+                                        <input type="text" value={details._id} readOnly placeholder="info@site.com" className="input input-bordered" />
+                                    </label>
+                                </div><div className="form-control">
+                                    <label className="input-group">
+                                        <span>Donator email</span>
+                                        <input type="text" value={details.email} readOnly placeholder="info@site.com" className="input input-bordered" />
+                                    </label>
+                                </div><div className="form-control">
+                                    <label className="input-group">
+                                        <span> Donator Name</span>
+                                        <input type="text" value={details.name} readOnly placeholder="info@site.com" className="input input-bordered" />
+                                    </label>
+                                </div><div className="form-control">
+                                    <label className="input-group">
+                                        <span>User email</span>
+                                        <input type="text" value={user.email} readOnly placeholder="info@site.com" className="input input-bordered" />
+                                    </label>
+                                </div><div className="form-control">
+                                    <label className="input-group">
+                                        <span>Request Date</span>
+                                        <input type="text" value={getCurrentDate()} placeholder="info@site.com" className="input input-bordered" />
+                                    </label>
+                                </div><div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Your Email</span>
+                                    </label>
+                                    <label className="input-group">
+                                        <span>Email</span>
+                                        <input type="text" placeholder="info@site.com" className="input input-bordered" />
+                                    </label>
+                                </div><div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Your Email</span>
+                                    </label>
+                                    <label className="input-group">
+                                        <span>Email</span>
+                                        <input type="text" placeholder="info@site.com" className="input input-bordered" />
+                                    </label>
+                                </div>
+                                <div className="modal-action">
+                                    <a href="#" className="btn">Yay!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
